1bda4baf4fbf6986d24f2f99b936c2d9b6d62efa
Gina Häußge <gina@octoprint.org>
2021-01-14 18:10:45 +0100

    :sparkles: First throw at issue validator

---
+++ issue-validation/README.md
@@ -0,0 +1,106 @@
+# Issue Validation Action
+
+This action performs some validation checks against incoming issues.
+
+## Inputs
+
+### `repo-token`
+
+**Required** The GitHub token to use, should usually be set to `secrets.GITHUB_TOKEN`
+
+### `configuration-path`
+
+The path to the yml configuration file.
+
+## Usage
+
+`.github/workflows/issue-validation.yml`:
+
+```yaml
+name: "Issue validator"
+on:
+  issues:
+    types: ["opened", "edited"]
+
+jobs:
+  validate:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: OctoPrint/actions/issue-validation@main
+      with:
+        repo-token: "${{ secrets.GITHUB_TOKEN }}"
+```
+
+`.github/issue-validation.yml`:
+
+```yaml
+approve_label: approved
+problem_label: incomplete
+ignored_labels: 
+  - bug
+  - request
+  - improvement
+  - brainstorming
+  - task
+  - rc feedback
+  - awaiting information
+  - not octoprint
+  - approved
+  - done
+ignored_titles:
+  - '[Request]'
+  - '[Brainstorming]'
+  - '[RC Feedback]'
+  - 'Feature Request'
+ignored_authors:
+  - @octoprint/octoprint-maintainers
+  - @octoprint/octoprint-triage
+  - sentry-io
+  - FormerLurker
+  - bzed
+
+text_reminder: |
+  Hi @@AUTHOR@@, 
+  
+  it looks like there is some **information missing** from your bug report that will
+  be needed in order to solve the problem. Read the [Contribution Guidelines](https://github.com/foosel/OctoPrint/blob/master/CONTRIBUTING.md)
+  which will provide you with a template to fill out here so that your bug report
+  is ready to be investigated (I promise I'll go away then too!).
+
+  If you did not intend to report a bug but wanted to **request a feature or brain
+  storm** about some kind of development, please take special note of the title format
+  to use as described in the [Contribution Guidelines](https://github.com/foosel/OctoPrint/blob/master/CONTRIBUTING.md).
+
+  **Please do not abuse the bug tracker as a support forum** - that can be found at
+  [community.octoprint.org](https://community.octoprint.org). Go there for any kind
+  of issues with network connectivity, webcam functionality, printer detection or
+  any other kind of such support requests or general questions.
+
+  Also **make sure you are at the right place** - this is the bug tracker of the official
+  version of OctoPrint, not the Raspberry Pi image OctoPi nor any unbundled third
+  party OctoPrint plugins or unofficial versions. Make sure too that you have **read
+  through the [Frequently Asked Questions](http://faq.octoprint.org)** and searched
+  the [**existing tickets**](https://github.com/foosel/OctoPrint/search?q=&ref=cmdform&type=Issues)
+  for your problem - try multiple search terms please.
+
+  I'm marking this one now as needing some more information. Please understand that
+  if you do not provide that information within the next two weeks
+  I'll close this ticket so it doesn't clutter the bug tracker. This is nothing
+  personal, it's needed to keep this project manageable. Please just be considerate 
+  and help the maintainers solve this problem
+  quickly by following the guidelines linked above. Remember, the less time the devs
+  have to spend running after information on tickets, the more time they have to actually
+  solve problems and add awesome new features. Thank you!
+
+  *I'm just a bot 🤖, not a human being, so don't expect any replies
+  from me :) Your ticket is read by humans too, I'm just not one of them.*
+
+
+text_closing: |
+  Since apparently some of the required information is still missing, I'm
+  closing this now, sorry. Feel free to request a reopen of this or create a new issue
+  once you can provide **all** [required information](https://github.com/foosel/OctoPrint/blob/master/CONTRIBUTING.md#how-to-file-a-bug-report).
+
+  This is nothing personal. Thank you for your collaboration.
+
+```
---
+++ issue-validation/src/index.js
@@ -0,0 +1,199 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const yaml = require('js-yaml');
+
+function getIssueNumber() {
+  const issue = github.context.payload.issue;
+  if (!issue) {
+    return undefined;
+  }
+
+  return issue.number;
+}
+
+async function fetchContent(client, path) {
+  const response = await client.repos.getContent({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    path: path,
+    ref: github.context.sha
+  });
+
+  return Buffer.from(response.data.content, response.data.encoding).toString();
+}
+
+async function readConfig(client, path) {
+  const content = await fetchContent(client, path);
+
+  const config = yaml.load(content);
+
+  return config;
+}
+
+async function matchesUser(client, user, check) {
+  if (check.startsWith("@")) {
+    if (check.includes("/")) {
+      return await isMemberOfTeam(client, user, check);
+    } else {
+      return await isMemberOfOrg(client, user, check);
+    }
+  } else {
+    return user === check;
+  }
+}
+
+async function isMemberOfOrg(client, user, org) {
+  if (org.startsWith("@")) {
+    org = org.slice(1);
+  }
+
+  const { data: orgs } = await client.orgs.listForUser({ username, per_page: 100 });
+  return orgs.some((name) => name === org);
+}
+
+async function isMemberOfTeam(client, user, team) {
+  if (!team.includes("/")) {
+    return false;
+  }
+  if (team.startsWith("@")) {
+    team = team.slice(1);
+  }
+
+  let org;
+  [org, team] = team.split("/", 1);
+
+  const query = `query($cursor: String, $org, String!, $userLogins: [String!], $username: String!) {
+    user(login: $username) {
+      id
+    }
+    organization(login: $org) {
+      teams (first:1, userLogins: $userLogins, after: $cursor) {
+        nodes {
+          name
+        }
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+      }
+    }
+  }`
+
+  let data;
+  let teams = [];
+  let cursor = null;
+
+  do {
+    data = await client.graphql(query, {
+      "cursor": cursor,
+      "org": org,
+      "userLogins": [user],
+      "username": user
+    });
+    
+    teams = teams.concat(data.organization.teams.nodes.map((val) => val.name));
+    cursor = data.organization.teams.pageInfo.endCursor;
+  } while (!teams.includes(team) && data.organization.teams.pageInfo.hasNextPage);
+
+  return teams.includes(team);
+}
+
+function isIgnored(client, issue, config) {
+  const labels = issue.labels.map((val) => val.name);
+  const title = issue.title;
+  const author = issue.user.login;
+
+  const ignored_labels = config.ignored_labels || [];
+  const ignored_titles = config.ignored_titles || [];
+  const ignored_authors = config.ignored_authors || [];
+
+  return ignored_labels.some((l) => labels.includes(l))
+      || ignored_titles.some((t) => title.includes(t))
+      || ignored_authors.some((a) => matchesUser(client, author, a));
+}
+
+function checkIssue(issue, config) {
+  const phrase = config.keyphrase;
+
+  if (!phrase) {
+    return true;
+  }
+
+  return issue.body.includes(phrase);
+}
+
+async function run() {
+  try {
+
+    const token = core.getInput("repo-token", { required: true });
+    const configPath = core.getInput("configuration-path", { required: true });
+
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+
+    const number = getIssueNumber();
+    if (!number) {
+      console.log("Could not get issue number from context, exiting");
+      return;
+    }
+
+    const client = new github.getOctokit(token);
+
+    const config = await readConfig(client, configPath);
+    if (!config) {
+      console.log("Could not get configuration from repository, existing");
+      return;
+    }
+    
+    // Get current issue data (the data in the context might be outdated)
+    const { data: issue } = await client.issues.get({
+      owner: owner,
+      repo: repo,
+      issue_number: number
+    });
+
+    if (isIgnored(client, issue, config)) {
+      console.log("Issue is ignored by validation");
+      return;
+    }
+
+    if (!checkIssue(issue)) {
+      // something's missing here, label & comment accordingly
+      if (config.problem_label) {
+        client.issues.addLabels({
+          owner: owner,
+          repo: repo,
+          issue_number: number,
+          labels: [config.problem_label]
+        });
+      }
+
+      if (config.text_reminder) {
+        client.issues.createComment({
+          owner: owner,
+          repo: repo,
+          issue_number: number,
+          body: config.text_reminder.replace("@@AUTHOR@@", issue.user.login)
+        });
+      }
+    
+      core.setFailed("This issue has not passed validation");
+    } else {
+      // mark as approved
+      if (config.problem_label) {
+        client.issues.removeLabel({
+          owner: owner,
+          repo: repo,
+          issue_number: number,
+          label: config.problem_label
+        });
+      }
+    }
+  
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
