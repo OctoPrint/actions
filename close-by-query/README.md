72d1ded117ff8476c51566857fbaec3220946550
Gina Häußge <gina@octoprint.org>
2021-01-15 13:31:28 +0100

    :sparkles: Initial throw at cleanup action

---
+++ close-by-query/README.md
@@ -0,0 +1,52 @@
+# PR Validation Action
+
+This action performs some validation checks against incoming PRs.
+
+It will check whether a non-empty description has been provided and
+whether a set of allowed PR targets and forbidden PR sources is matched
+which can optionally be modified based on labels already applied on
+the PR through earlier steps.
+
+## Inputs
+
+### `repo-token`
+
+**Required** The GitHub token to use, should usually be set to `secrets.GITHUB_TOKEN`
+
+### `query`
+
+**Required** The query to run
+
+### `comment`
+
+A comment to post upon closing. No comment will be posted if left unset.
+
+## Usage
+
+`.github/workflows/issue-cleanup.yml`:
+
+```yaml
+name: "Issue cleanup"
+on:
+  schedule:
+    - cron: "0 0 * * *"
+
+jobs:
+  cleanup:
+    runs-on: ubuntu-latest
+    steps:
+    - id: date
+      run: |
+        echo "::set-output name=CUTOFF::`date --date='14 days ago' +'%Y-%m-%d'`"
+    - uses: OctoPrint/actions/issue-validation@main
+      with:
+        repo-token: "${{ secrets.GITHUB_TOKEN }}"
+        query: 'is:issue is:open label:incomplete created:<${{ steps.date.outputs.CUTOFF }}'
+        comment: |
+          Since apparently some of the required information is still missing, this will be
+          closed now, sorry. Feel free to request a reopen of this or create a new issue
+          once you can provide **all** 
+          [required information](https://github.com/OctoPrint/OctoPrint/blob/master/CONTRIBUTING.        md#how-to-file-a-bug-report).
+        
+          This is nothing personal. Thank you for your collaboration.
+```
---
+++ close-by-query/src/index.js
@@ -0,0 +1,95 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function search(client, query, per_page) {
+  per_page = per_page || 100;
+
+  let page = 1;
+  let issues = [];
+  let result;
+
+  try {
+    do {
+      result = await client.search.issuesAndPullRequests({
+        "q": query,
+        "sort": "created",
+        "order": "asc",
+        "per_page": max,
+        "page": page
+      });
+
+      issues = issues.concat(result.data.items);
+      page += 1;
+    } while (result.data.incomplete_results);
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+  }
+
+  return issues;
+}
+
+async function cleanup_issues(client, query, comment, dryrun) {
+  try {
+
+    const owner = github.context.repo.owner;
+    const repo = github.context.repo.repo;
+
+    core.info("Running query: " + query);
+
+    const issues = await search(client, query);
+
+    core.info("Found " + issues.length + " issues to close");
+
+    for (const issue of issues) {
+      const number = issue.number;
+
+      if (dryrun) {
+        core.info("Would close issue #" + number);
+        continue;
+      }
+
+      if (comment) {
+        await client.issues.createComment({
+          owner: owner,
+          repo: repo,
+          issue_number: number,
+          body: comment
+        })
+      }
+
+      await client.issues.update({
+        owner: owner,
+        repo: repo,
+        issue_number: number,
+        state: "closed"
+      });
+
+      core.info("Closed #" + number);
+    };
+
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+  }
+}
+
+async function run() {
+  try {
+    const token = core.getInput("repo-token", { required: true });
+    const query = core.getInput("query", { required: true });
+    const comment = core.getInput("comment");
+    const dryrun = core.getInput("dry-run") || false;
+
+    const client = github.getOctokit(token);
+
+    if (github.context.eventName == "issues") {
+      cleanup_issues(client, query, comment, dryrun);
+    }
+  } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
